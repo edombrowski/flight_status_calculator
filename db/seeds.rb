e2aad8732075e1e4775efaa709591c7685a7218e
edombrowski <Katie@Katies-MacBook-Pro-3.local>
2015-06-12 00:28:53 -0500

    Fixed seeding
    fixed seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Program.create(t3_threshold: "100000", t3_name: "Executive Platinum", t2_threshold: "50000", t2_name: "Platinum", t1_threshold: "25000", t1_name: "Gold", program_type: "flight", program_name: "American Airlines AAdvantage", t4_name: "")
+
+Program.create(t3_threshold: "75000", t3_name: "Premier Platinum", t2_threshold: "50000", t2_name: "Premier Gold", t1_threshold: "25000", t1_name: "Premier Silver", program_type: "flight", program_name: "Unite MileagePlus", t4_name: "Premier 1K", t4_threshold: "100000")
+
+Program.create(t3_threshold: "75000", t3_name: "Premier Platinum", t2_threshold: "50000", t2_name: "Premier Gold", t1_threshold: "25000", t1_name: "Premier Silver", program_type: "flight", program_name: "Unite MileagePlus", t4_name: "Premier 1K", t4_threshold: "100000")
+
+User.create(email: "erik@example.com", password: "12341234", program_id: "1")
+User.create(email: "katie@Examples.com", password: "12341234", program_id: "1")
+User.create(email: "test@Examples.com", password: "12341234", program_id: "1")
+User.create(email: "erik.dombrowski@gmail.com", password: "12341234", program_id: "1")
+
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'airports.csv'))
@@ -21,4 +32,4 @@ csv.each do |row|
 puts "#{t.airport_code}, #{t.airport_name} saved"
 end
 
-puts "There are now #{Airport.count} rows in the airport table  "
+puts "There are now #{Airport.count} rows in the airport table"
